14f94256b162b48af8881dbcf84ab2d2067de049
Jothimani <jothimanineyveli2014@gmail.com>
2016-02-10 15:41:08 +0530

    [ADD] forum_questions: completed 2 questions

---
+++ forum_questions/__openerp__.py
@@ -0,0 +1,11 @@
+{
+	'name': 'Forum Questions',
+    'description': 'Odoo Forum Questions',
+    'category': 'Forum',
+    'version': '1.0',
+    'author': 'Jothimani.R',
+    'depends': ['product'],
+    'data': [
+        'views/views.xml',  
+    ],
+}
---
+++ forum_questions/models/models.py
@@ -0,0 +1,25 @@
+from openerp import models, fields, api
+# https://www.odoo.com/forum/help-1/question/create-custom-fields-and-set-alerts-on-it-97466
+class product_template_inherit(models.Model):
+	_inherit = 'product.template'
+
+	threshold = fields.Integer('Threshold')
+
+	def check_product_qty(self, cr, uid, context=None):
+		ids = self.search(cr, uid, [], context)
+		products = self.browse(cr, uid, ids, context)
+		for product in products:
+			if product.qty_available < product.threshold:
+				print '\n',product.name,'***',True
+
+
+
+# https://www.odoo.com/forum/help-1/question/concatenate-firstname-and-lastname-and-fill-into-name-field-97505
+# name = fields.Char(compute='_compute_name', store="True")
+# firstname = fields.Char()
+# lastname = fields.Char()
+
+# @api.depends('firstname', 'lastname')
+# def _compute_name(self):
+#     for record in self:
+#         record.name = record.firstname + record.lastname
