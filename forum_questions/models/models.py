bbf2c6f41ac6398b3d5a68cdc1676f30650c1940
Jothimani <jothimanineyveli2014@gmail.com>
2016-02-10 19:07:50 +0530

    2nd commit

--- forum_questions/models/models.py
+++ forum_questions/models/models.py
@@ -23,3 +23,18 @@ class product_template_inherit(models.Model):
 # def _compute_name(self):
 #     for record in self:
 #         record.name = record.firstname + record.lastname
+
+
+# https://www.odoo.com/forum/help-1/question/how-to-call-onchane-from-xmlrpc-in-odoo-97452
+# import xmlrpclib
+# url = 'http://localhost:8050'
+# db = 'b2b_quick_order_list_view'
+# username = 'admin'
+# password = 'admin'
+
+# models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+# sss = models.execute_kw(db, 1, password,'sale.order', 'onchange_partner_id',[[],[1]])
+# print sss
+
+# https://www.odoo.com/forum/help-1/question/add-current-user-to-a-session-97459
+# self.env['res.users'].browse(self._uid).partner_id
